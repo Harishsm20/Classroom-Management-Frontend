a2b4dd67fd98fb123f0f89ac33f9076bc971a4ad
Harishsm20 <harishsm865@gmail.com>
2024-08-12 12:22:17 +0530

    Default page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Navigate to="/login" />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/principal-dashboard" element={<PrincipalDashboard />} />
