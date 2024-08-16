2b5ba721dfe11693f81a458f7ac414ed5b8ef8c3
Harishsm20 <harishsm865@gmail.com>
2024-08-16 06:34:13 +0530

    Loading component

---
+++ src/pages/LoaderComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../css/Loader.css';
+
+const LoaderComponent = () => {
+  return (
+    <div>
+      <div className="loader-container">
+            <div className="loader"></div>
+        </div>
+    </div>
+  )
+}
+
+export default LoaderComponent
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { parseJwt } from '../service/jwtService';
+import LoaderComponent from './LoaderComponent';
 import '../css/Login.css';
 
 const apiBaseUrl = 'https://classroom-management-backend.onrender.com'  || 'http://localhost:5000';
@@ -10,10 +11,12 @@ console.log('API Base URL:', apiBaseUrl);
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false); 
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setLoading(true);
         try {
             const response = await axios.post(`${apiBaseUrl}/api/auth/login`, { email, password });
             const token = response.data.token;
@@ -39,24 +42,28 @@ const Login = () => {
         <div className="login-container">
             <div className="login-form">
                 <h1 className="project-title">Classroom Management</h1>
-                <form onSubmit={handleSubmit}>
-                    <h2>Login</h2>
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                    <button type="submit">Login</button>
-                </form>
+                {loading ? (
+                    <LoaderComponent />
+                ) : (
+                    <form onSubmit={handleSubmit}>
+                        <h2>Login</h2>
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                        <button type="submit">Login</button>
+                    </form>
+                )}
             </div>
         </div>
     );
